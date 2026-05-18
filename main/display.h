bf0f22dc7510eb6d098ae2205dae008f858d8195
Your Name <you@example.com>
2026-05-18 16:15:32 +0530

    track: add display abstraction layer and bitmap font

---
+++ main/display.h
@@ -0,0 +1,27 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "esp_err.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    DISPLAY_BOOT,
+    DISPLAY_READY,
+    DISPLAY_PAYMENT_RECEIVED,
+    DISPLAY_ERROR
+} display_state_t;
+
+typedef enum {
+    DISPLAY_QR_WIFI,
+    DISPLAY_QR_PORTAL
+} display_qr_mode_t;
+
+esp_err_t display_init(void);
+void display_set_state(display_state_t state);
+void display_update(const char *ap_ssid, int active_clients,
+                    uint64_t wallet_balance, const char *portal_url);
+void display_render_text(int x, int y, const char *text, uint16_t fg, uint16_t bg, int scale);
+void display_render_qr(const char *text);
+
+#endif
---
+++ main/font.h
@@ -0,0 +1,11 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include <stdint.h>
+
+#define FONT_GLYPH_W 8
+#define FONT_GLYPH_H 8
+
+extern const uint8_t font8x8_basic[128][8];
+
+#endif
