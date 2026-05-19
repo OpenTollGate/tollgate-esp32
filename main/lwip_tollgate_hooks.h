4f713120dbedd37a3512c2ec1af0766025a59d57
Your Name <you@example.com>
2026-05-20 02:12:13 +0530

    fix: revert lwip hook to use main/firewall.c function
    tollgate_core_ip4_canforward_filter is in a separate component that
    lwip can't link against. Use the existing tollgate_ip4_canforward_filter
    from main/firewall.c instead.

--- main/lwip_tollgate_hooks.h
+++ main/lwip_tollgate_hooks.h
@@ -3,8 +3,8 @@
 
 #include "lwip/pbuf.h"
 
-int tollgate_core_ip4_canforward_filter(struct pbuf *p, u32_t dest_addr_hostorder);
+int tollgate_ip4_canforward_filter(struct pbuf *p, u32_t dest_addr_hostorder);
 
-#define LWIP_HOOK_IP4_CANFORWARD(p, addr) tollgate_core_ip4_canforward_filter(p, addr)
+#define LWIP_HOOK_IP4_CANFORWARD(p, addr) tollgate_ip4_canforward_filter(p, addr)
 
 #endif
